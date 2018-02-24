96c1427632f00a433b565ef31ce7ed1d6a74870e
Renato Samperio <renatosamperio@gmail.com>
2018-02-24 19:03:58 +0100

    SETUP_INSTALL: Added file

---
+++ setup/install.bash
@@ -0,0 +1,16 @@
+
+## Making BIN in system
+export INSTALL_HS=/opt/home_services/src/home_services
+ln -s $INSTALL_HS/setup/bin/hs_finder_trigger /usr/local/bin/hs_finder_trigger
+ln -s $INSTALL_HS/setup/bin/hs_torrent_finder /usr/local/bin/hs_torrent_finder
+ln -s $INSTALL_HS/setup/bin/hs_torrent_search /usr/local/bin/hs_torrent_search
+ln -s $INSTALL_HS/setup/bin/hs_torrent_trigger /usr/local/bin/hs_torrent_trigger
+
+sudo cp $INSTALL_HS/setup/etc/systemd/system/hs_torrent_search.service /etc/systemd/system
+sudo cp $INSTALL_HS/setup/etc/systemd/system/hs_torrent_finder.service /etc/systemd/system
+sudo systemctl enable hs_torrent_search.service
+sudo systemctl enable hs_torrent_finder.service
+sudo systemctl restart hs_torrent_search.service
+sudo systemctl restart hs_torrent_finder.service
+
+sudo systemctl daemon-reload
